c75a85e5de5aca0d2d751495795c4e13db497539
tomasz.piotrowicz <tomasz.piotrowicz@amano.ai>
2023-01-10 18:07:28 +0100

    fix

--- pages/produkt/[id].tsx
+++ pages/produkt/[id].tsx
@@ -1,7 +1,6 @@
 import {getProductById, Product} from "../../api/products";
 import {GetServerSideProps} from "next";
 import Image from "next/image";
-import styles from "../../styles/ProductDetail.module.scss"
 
 export type ProductDetailProps = {
     product: Product
@@ -20,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params}) => {
 const ProductDetail = ({product}: ProductDetailProps): JSX.Element => {
     return (
         <>
-            <div className={styles.container}>
+            <div>
                 <div>{product.title}</div>
                 <div>
                     <Image width={200} height={200} src={product.images[0]} alt={product.title} />
